197e4257491fb43acc0c091538e3517473001166
Nicolas.pino <nicolaspino818@gmail.com>
2023-10-23 22:36:42 -0300

    a

--- hostal/settings.py
+++ hostal/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hostal',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
