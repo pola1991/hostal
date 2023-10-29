37103ef3fd45d80c1f54f6ee37772dcef90864e6
nehe18 <103059887+nehe18@users.noreply.github.com>
2023-10-29 19:48:18 -0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -11,5 +11,9 @@ urlpatterns = [
     path('addProveedor/', addProveedor, name='addProveedor'),
     path('readProveedor/', readProveedor, name='readProveedor'),
     path('eliminarProveedor/<id>/',eliminarProveedor,name='eliminarProveedor'),
+    path('hab',hab,name='hab'),
+    path('formu_create',formu_create,name='formu_create'),
+    path('formu_update/<int:id>', formu_update, name='formu_update'),
+    path('formu_delete/<int:id>/', formu_delete, name='formu_delete'),
     
 ]
