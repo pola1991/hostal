8f8b5df42bd22712d440786ba1fb264ef04b459b
Nicolas.pino <nicolaspino818@gmail.com>
2023-11-01 22:58:54 -0300

    cierre de sesion y logins

---
+++ testing/features/LoginUsuarioIncorrecto.feature
@@ -0,0 +1,6 @@
+Feature: Ingreso de usuario al sistema
+Scenario: Inicio de sesion incorrecto (Usuario)
+    Given Accedo a la url para hacer login con usuario incorrecto "http://127.0.0.1:8000/"
+    When hago click en enlace para hacer inicio de sesion con usuario incorrecto "//*[@id="navbarNavDropdown"]/ul/li[3]/a"
+    And Ingresa las credenciales de acceso con usuario incorrecto
+    Then Recibo mensaje inicio de sesion incorrecto (usuario)
--- testing/steps/PasosLoginUsuarioIncorrecto.py
+++ testing/steps/PasosLoginUsuarioIncorrecto.py
@@ -5,24 +5,24 @@ from conexion import webapp
 driver = webapp.get_driver()
 
 
-@given(u'Accedo a la url para hacer login incorrecto "{url}"')
+@given(u'Accedo a la url para hacer login con usuario incorrecto "{url}"')
 def step_acceso_a_la_url(context,url):
     driver.get(url)
 
-@when(u'hago click en enlace para hacer inicio de sesion incorrecto "{link}"')
+@when(u'hago click en enlace para hacer inicio de sesion con usuario incorrecto "{link}"')
 def localiza_enlace_login(context,link):
     driver.implicitly_wait(5)
     driver.find_element(By.XPATH,link).click()
     
 
-@when(u'Ingresa las credenciales de acceso incorrecto')
+@when(u'Ingresa las credenciales de acceso con usuario incorrecto')
 def ingresa_credenciales(context):
     driver.find_element(By.XPATH,"//*[@id='id_username']").clear()
-    driver.find_element(By.XPATH,"//*[@id='id_username']").send_keys("paula")
-    driver.find_element(By.NAME,'password').send_keys('1234')
+    driver.find_element(By.XPATH,"//*[@id='id_username']").send_keys("pola")
+    driver.find_element(By.NAME,'password').send_keys('12345678')
     driver.find_element(By.XPATH,"/html/body/div/div/div/div/div[2]/form/div[3]/input").click()
 
-@then(u'Recibo mensaje inicio de sesion incorrecto')
+@then(u'Recibo mensaje inicio de sesion incorrecto (usuario)')
 def ingresa_a_la_cuenta(context):
     if(driver.find_element(By.XPATH,"/html/body/div/div/div/div/div[2]/form/div[1]").is_displayed()):
         print('login correcto')
